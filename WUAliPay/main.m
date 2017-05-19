35a0a4302465c90795182c4c9969859362bfd5a8
醒来－技术 <1020166296@qq.com>
2017-05-19 10:51:48 +0800

    init

---
+++ WUAliPay/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WUAliPay
+//
+//  Created by 醒来－技术 on 2017/5/19.
+//  Copyright © 2017年 wakeup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
